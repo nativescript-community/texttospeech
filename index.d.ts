2ffbafd4762153884967a1f2131c4aacb292d265
Brad Martin <bradwaynemartin@gmail.com>
2017-06-14 13:06:50 -0500

    add prettier

--- index.d.ts
+++ index.d.ts
@@ -1,40 +1,32 @@
-
 export declare class TNSTextToSpeech {
-
-    /**
+  /**
      * Initiate the text to speech.
      * @param {object} SpeakOptions - SpeakOptions object.
      */
-    speak(options: SpeakOptions): void;
+  speak(options: SpeakOptions): void;
 
-
-    /**
+  /**
      * Release the resources used by the TextToSpeech engine/synthesizer
      */
-    destroy(): void;
+  destroy(): void;
 
-
-    /**
+  /**
      * Pause the engine/synthesizer currently speaking.
      */
-    pause(): void;
+  pause(): void;
 
-
-    /**
+  /**
      * Resume the engine/synthesizer. On Android it will start from beginning - since there is no actual pause, only stop.
      */
-    resume(): void;
-
+  resume(): void;
 }
 
-
-
 export interface SpeakOptions {
-    text: string;
-    queue?: boolean;
-    pitch?: number;
-    speakRate?: number;
-    volume?: number;
-    language?: string;
-    finishedCallback?: Function;
+  text: string;
+  queue?: boolean;
+  pitch?: number;
+  speakRate?: number;
+  volume?: number;
+  language?: string;
+  finishedCallback?: Function;
 }
--- texttospeech.ios.ts
+++ texttospeech.ios.ts
@@ -1,36 +1,39 @@
-import { SpeakOptions } from './index.d.ts';
+import { SpeakOptions } from "./index.d.ts";
 
-declare var NSObject, AVSpeechUtterance, AVSpeechSynthesizer, AVSpeechSynthesisVoice, AVSpeechBoundary, AVSpeechSynthesizerDelegate: any;
+declare var NSObject,
+  AVSpeechUtterance,
+  AVSpeechSynthesizer,
+  AVSpeechSynthesisVoice,
+  AVSpeechBoundary,
+  AVSpeechSynthesizerDelegate: any;
 
 let delegate;
 let doneCallback;
 
 class MySpeechDelegate extends NSObject {
-	public static ObjCProtocols = [AVSpeechSynthesizerDelegate];
+  public static ObjCProtocols = [AVSpeechSynthesizerDelegate];
 
-	speechSynthesizerDidStartSpeechUtterance(synthesizer, utterance) {
-		// TODO
-	}
+  speechSynthesizerDidStartSpeechUtterance(synthesizer, utterance) {
+    // TODO
+  }
 
-	speechSynthesizerDidFinishSpeechUtterance(synthesizer, utterance) {
-		if (doneCallback) {
-			doneCallback();
-		}
+  speechSynthesizerDidFinishSpeechUtterance(synthesizer, utterance) {
+    if (doneCallback) {
+      doneCallback();
+    }
+  }
 
-	}
+  speechSynthesizerDidPauseSpeechUtterance(synthesizer, utterance) {
+    // TODO
+  }
 
-	speechSynthesizerDidPauseSpeechUtterance(synthesizer, utterance) {
-		// TODO
-	}
+  speechSynthesizerDidContinueSpeechUtterance(synthesizer, utterance) {
+    // console.log("Continued speaking");
+  }
 
-	speechSynthesizerDidContinueSpeechUtterance(synthesizer, utterance) {
-		// console.log("Continued speaking");
-	}
-
-	speechSynthesizerDidCancelSpeechUtterance(synthesizer, utterance) {
-		// console.log("Cancelled speaking");
-	}
-
+  speechSynthesizerDidCancelSpeechUtterance(synthesizer, utterance) {
+    // console.log("Cancelled speaking");
+  }
 }
 
 // , {
@@ -40,111 +43,118 @@ class MySpeechDelegate extends NSObject {
 // 		protocols: [AVSpeechSynthesizerDelegate]
 // };
 
-
 export class TNSTextToSpeech {
+  private _speechSynthesizer: any; /// AVSpeechSynthesizer
 
-	private _speechSynthesizer: any; /// AVSpeechSynthesizer
+  public speak(options: SpeakOptions) {
+    if (!this._speechSynthesizer) {
+      this._speechSynthesizer = AVSpeechSynthesizer.alloc().init();
+      this._speechSynthesizer.delegate = new MySpeechDelegate();
+    }
 
+    if (!this.isString(options.text)) {
+      console.log("Text is required to speak.");
+      return;
+    }
 
-	public speak(options: SpeakOptions) {
-		if (!this._speechSynthesizer) {
-			this._speechSynthesizer = AVSpeechSynthesizer.alloc().init();
-			this._speechSynthesizer.delegate = new MySpeechDelegate();
-		}
+    doneCallback = options.finishedCallback;
 
-		if (!this.isString(options.text)) {
-			console.log("Text is required to speak.");
-			return;
-		}
+    // valid values for pitch are 0.5 to 2.0
+    if (!this.isNumber(options.pitch)) {
+      options.pitch = 1.0;
+    } else if (options.pitch < 0.5) {
+      options.pitch = 0.5;
+    } else if (options.pitch > 2.0) {
+      options.pitch = 2.0;
+    }
 
-		doneCallback = options.finishedCallback;
+    // valid values are AVSpeechUtteranceMinimumSpeechRate to AVSpeechUtteranceMaximumSpeechRate
+    if (!this.isNumber(options.speakRate)) {
+      options.speakRate =
+        AVSpeechUtterance.AVSpeechUtteranceMaximumSpeechRate / 4.0; // default rate is way too fast
+    } else if (
+      options.speakRate < AVSpeechUtterance.AVSpeechUtteranceMinimumSpeechRate
+    ) {
+      options.speakRate = AVSpeechUtterance.AVSpeechUtteranceMinimumSpeechRate;
+    } else if (
+      options.speakRate > AVSpeechUtterance.AVSpeechUtteranceMaximumSpeechRate
+    ) {
+      options.speakRate = AVSpeechUtterance.AVSpeechUtteranceMaximumSpeechRate;
+    }
 
-		// valid values for pitch are 0.5 to 2.0
-		if (!this.isNumber(options.pitch)) {
-			options.pitch = 1.0;
-		} else if (options.pitch < 0.5) {
-			options.pitch = 0.5;
-		} else if (options.pitch > 2.0) {
-			options.pitch = 2.0;
-		}
+    // valid values for volume are 0.0 to 1.0
+    if (!this.isNumber(options.volume) || options.volume > 1.0) {
+      options.volume = 1.0;
+    } else if (options.volume < 0.0) {
+      options.volume = 0.0;
+    }
 
-		// valid values are AVSpeechUtteranceMinimumSpeechRate to AVSpeechUtteranceMaximumSpeechRate
-		if (!this.isNumber(options.speakRate)) {
-			options.speakRate = AVSpeechUtterance.AVSpeechUtteranceMaximumSpeechRate / 4.0; // default rate is way too fast
-		} else if (options.speakRate < AVSpeechUtterance.AVSpeechUtteranceMinimumSpeechRate) {
-			options.speakRate = AVSpeechUtterance.AVSpeechUtteranceMinimumSpeechRate;
-		} else if (options.speakRate > AVSpeechUtterance.AVSpeechUtteranceMaximumSpeechRate) {
-			options.speakRate = AVSpeechUtterance.AVSpeechUtteranceMaximumSpeechRate;
-		}
+    let speechUtterance = AVSpeechUtterance.alloc().initWithString(
+      options.text
+    );
 
-		// valid values for volume are 0.0 to 1.0
-		if (!this.isNumber(options.volume) || options.volume > 1.0) {
-			options.volume = 1.0;
-		} else if (options.volume < 0.0) {
-			options.volume = 0.0;
-		}
+    if (
+      this.isString(options.language) &&
+      this.isValidLocale(options.language)
+    ) {
+      speechUtterance.voice = AVSpeechSynthesisVoice.voiceWithLanguage(
+        options.language
+      );
+    }
 
-		let speechUtterance = AVSpeechUtterance.alloc().initWithString(options.text);
+    speechUtterance.pitchMultiplier = options.pitch;
+    speechUtterance.volume = options.volume;
+    speechUtterance.rate = options.speakRate;
 
-		if (this.isString(options.language) && this.isValidLocale(options.language)) {
-			speechUtterance.voice = AVSpeechSynthesisVoice.voiceWithLanguage(options.language);
-		}
+    if (!this.isBoolean(options.queue)) {
+      options.queue = false;
+    }
 
-		speechUtterance.pitchMultiplier = options.pitch;
-		speechUtterance.volume = options.volume;
-		speechUtterance.rate = options.speakRate;
+    if (!options.queue && this._speechSynthesizer.speaking) {
+      this._speechSynthesizer.stopSpeakingAtBoundary(
+        AVSpeechBoundary.AVSpeechBoundaryImmediate
+      );
+    }
 
-		if (!this.isBoolean(options.queue)) {
-			options.queue = false;
-		}
+    this._speechSynthesizer.speakUtterance(speechUtterance);
+  }
 
-		if (!options.queue && this._speechSynthesizer.speaking) {
-			this._speechSynthesizer.stopSpeakingAtBoundary(AVSpeechBoundary.AVSpeechBoundaryImmediate);
-		}
+  public pause(now) {
+    this._speechSynthesizer.pauseSpeakingAtBoundary(
+      now
+        ? AVSpeechBoundary.AVSpeechBoundaryImmediate
+        : AVSpeechBoundary.AVSpeechBoundaryWord
+    );
+  }
 
-		this._speechSynthesizer.speakUtterance(speechUtterance);
-	}
+  public resume() {
+    this._speechSynthesizer.continueSpeaking();
+  }
 
+  public destroy() {
+    /// does iOS have anything to destroy/free resources???
+    this._speechSynthesizer = null;
+  }
 
-	public pause(now) {
-		this._speechSynthesizer.pauseSpeakingAtBoundary(now ? AVSpeechBoundary.AVSpeechBoundaryImmediate : AVSpeechBoundary.AVSpeechBoundaryWord);
-	}
-
-	public resume() {
-		this._speechSynthesizer.continueSpeaking();
-	}
-
-	public destroy() {
-		/// does iOS have anything to destroy/free resources???
-		this._speechSynthesizer = null;
-	}
-
-
-
-
-
-	/**
+  /**
 	 * Private Methods
 	 */
 
-	private isString(elem) {
-		return Object.prototype.toString.call(elem).slice(8, -1) === 'String';
-	};
+  private isString(elem) {
+    return Object.prototype.toString.call(elem).slice(8, -1) === "String";
+  }
 
-	private isBoolean(elem) {
-		return Object.prototype.toString.call(elem).slice(8, -1) === 'Boolean';
-	};
+  private isBoolean(elem) {
+    return Object.prototype.toString.call(elem).slice(8, -1) === "Boolean";
+  }
 
-	private isNumber(elem) {
-		return Object.prototype.toString.call(elem).slice(8, -1) === 'Number';
-	};
+  private isNumber(elem) {
+    return Object.prototype.toString.call(elem).slice(8, -1) === "Number";
+  }
 
-	// helper function to test whether language code has valid syntax
-	private isValidLocale(locale) {
-		let re = new RegExp("\\w\\w-\\w\\w");
-		return re.test(locale);
-	}
-
-
-
-}	
+  // helper function to test whether language code has valid syntax
+  private isValidLocale(locale) {
+    let re = new RegExp("\\w\\w-\\w\\w");
+    return re.test(locale);
+  }
+}
