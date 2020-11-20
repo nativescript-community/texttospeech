75028834492dead463ef8ad4cb39a6bbd4a39cf9
Martin Guillon <martin.guillon@akylas.fr>
2020-11-20 10:34:39 +0100

    test: demo app

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -3,6 +3,6 @@ import { HelloWorldModel } from './main-view-model';
 
 // Event handler for Page "navigatingTo" event attached in main-page.xml
 export function navigatingTo(args: EventData) {
-  const page = args.object as Page;
-  page.bindingContext = new HelloWorldModel(page);
+    const page = args.object as Page;
+    page.bindingContext = new HelloWorldModel(page);
 }
